3c56b2b6597a90794b8f4695fa88d00256e30cc9
Tristanic07 <alainlado@gmail.com>
2023-07-16 17:15:23 +0800

    about

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,12 +1,12 @@
 import { FaJava, FaReact, FaPhp, FaJs, FaPython } from "react-icons/fa6";
 import { SiMysql } from "react-icons/si";
-import Alain from "../assets/image/alain1.jpg";
+import alain from "../assets/image/alain1.jpg";
 
 export default function About() {
   return (
     <div className="flex flex-col items-center gap-7 py-32">
       <div>
-        <img src={Alain} className="w-72" />
+        <img src={alain} className="w-72" />
       </div>
       <h1 className="text-center font-tektur font-bold text-4xl">Skills</h1>
       <section className="grid grid-rows-3 grid-cols-2 md:h-96   md:grid-rows-2 md:grid-cols-3  gap-5 ">
